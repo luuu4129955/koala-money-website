37125d4a2074f1d3158f764c4a3735f47ddc9c9c
crystalpeng <crystalpeng1998@gmail.com>
2022-02-13 21:05:19 +0800

    deploy

---
+++ precache-manifest.78be876c0b6f456fb5c182693ddbd9d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2559956326ceb88e62fe",
+    "url": "/koala-money-website/css/app.839a5429.css"
+  },
+  {
+    "revision": "a2f0be76bb9cd5626b0996021562a935",
+    "url": "/koala-money-website/index.html"
+  },
+  {
+    "revision": "2559956326ceb88e62fe",
+    "url": "/koala-money-website/js/app.5c132ce1.js"
+  },
+  {
+    "revision": "559bcdc290bea3236a26",
+    "url": "/koala-money-website/js/chunk-vendors.84b0f6ec.js"
+  },
+  {
+    "revision": "da108470304d83916b097a87b0713e0d",
+    "url": "/koala-money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/koala-money-website/robots.txt"
+  }
+]);
